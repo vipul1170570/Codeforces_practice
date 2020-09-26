27bd1dae5bd954ee9f970a79fcceabbbb84c94d6
vipul1170570 <48806038+vipul1170570@users.noreply.github.com>
2020-09-27 02:39:30 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,66 +1,54 @@
-#include<bits/stdc++.h>
-#include<unordered_set>
-#define initdp(a,b) for(int i=0;i<=a;i++)for(int j=0;j<=b;j++)dp[i][j]=-1;
-#define fi first
-#define se second
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
 #define pb push_back
-#define pii pair<int,int>
+#define mpr make_pair
+#define pii pair<int, int>
 #define ll long long
-#define pll pair<ll,ll>
+#define ld long double
 #define all(arr) arr.begin(), arr.end()
-#define rep(i,n) for(int i=0;i<n;i++)
-#define repd(i,n) for(int i=n-1;i>=0;i--)
-#define fo(i,l,r) for(int i=l;i<=r;i++)
-#define inf 1000000001
-#define inf1 1000000000000000001
-#define mod 1000000007
-#define pie 3.14159265358979323846
-#define N 1000005
-#define mid(l,r) l+(r-l)/2
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define fo(i, l, r) for (int i = l; i <= r; i++)
+#define MOD 1000000007
+#define mod 998244353
+#define pie 3.14159265358979323846264338327950L
+#define N 200009
+#define M 300009
+#define mid(l, r) l + (r - l) / 2
 #define vec vector<int>
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-#define end cout<<endl;
+#define umap unordered_map<int, int>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define endl "\n"
+#define int long long
 using namespace std;
+ 
+int dx[4]={1,0,-1,0},dy[4]={0,1,0,-1};  
+int ddx[8]={1,1,0,-1,-1,-1,0,1},ddy[8]={0,1,1,1,0,-1,-1,-1};
+int gcd(int a,int b){ if(!a)return b;return gcd(b%a,a);}
+int lcm(int a, int b) { return (a*b)/ gcd(a,b);}
+ 
 
-bool visited[N];
-int child[N], depth[N];
-vector <int> adj[N];
-
-inline int gcd(int a, int b) {
-	while (a != 0 && b != 0) {
-		if (a > b) {
-			a %= b;
-		} else {
-			b %= a;
-		}
-	}
-	return max(a, b);
-}
-
-void dfs(int u) {
-	visited[u]=true;
-	child[u]=0;
-	for(auto i: adj[u]) {
-		if(!visited[i]) {
-			depth[i] = depth[u]+1;
-			dfs(i);
-			child[u] += (1+child[i]);
-		}
-	}
-}
-
-void solve()
-{
+void test_case() {
     
 }
-int main()
+    
+
+signed main ()
 {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int t=1;
-    cin>>t;
-    while(t--)
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed << setprecision(20);
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
     {
-        
+        test_case();
+       // cout<<endl;
     }
 }
